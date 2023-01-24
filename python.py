1d45c33cb5744cbf91f371971e2a01c1c1005318
Divyanshu raj <98219015+its-divyanshu-raj@users.noreply.github.com>
2023-01-24 12:54:08 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,12 @@
+/* Welcome to the 
+1st module on Learn Python syntax. Some important points
+This learning path is geared to take you on the journey towards solving programming problems.
+The learning modules and content has been finalised based on the above goal.
+At the end of this learning path, you will be able to understand and interpret codes written in Python
+Please feel free to give us your feedback for each problem / solution / module.
+Help us get better.
+Let us write your first code in Python.
+The code in the editor outputs the sum of 2 and 3.
+Click on Submit to know the result. */ 
+
+print ( 2 + 3 ) 
